c6144a782c1245caaaf4d9e0a85b4d16ecb315c2
cyb3rf0x <rumbleincybertron@gmail.com>
2023-02-06 22:52:06 +0900

    Initial Commit

---
+++ Skrizzle/SkrizzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkrizzleApp.swift
+//  Skrizzle
+//
+//  Created by Derek Haus on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct SkrizzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
