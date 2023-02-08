5c0b91bdac080d0bcce35b73a52c9f975ffab34e
cyb3rf0x <rumbleincybertron@gmail.com>
2023-02-08 16:23:59 +0900

    update edit view

---
+++ Skrizzle/Views/ThemeView.swift
@@ -0,0 +1,20 @@
+//
+//  ThemeView.swift
+//  Skrizzle
+//
+//  Created by Derek Haus on 2023/02/08.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    var body: some View {
+        Text("Meh")
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView()
+    }
+}
