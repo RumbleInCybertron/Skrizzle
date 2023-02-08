358a874f277a0dc68d6f85e2432622e5436f6885
cyb3rf0x <rumbleincybertron@gmail.com>
2023-02-08 16:30:43 +0900

    add theme view

--- Skrizzle/Views/ThemeView.swift
+++ Skrizzle/Views/ThemeView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct ThemeView: View {
+    let theme: Theme
+    
     var body: some View {
-        Text("Meh")
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .fill(theme.mainColor)
+            Label(theme.name, systemImage: "paintpalette")
+                .padding(4)
+        }
+        .foregroundColor(theme.accentColor)
+        .fixedSize(horizontal: false, vertical: true)
     }
 }
 
 struct ThemeView_Previews: PreviewProvider {
     static var previews: some View {
-        ThemeView()
+        ThemeView(theme: .purple)
     }
 }
